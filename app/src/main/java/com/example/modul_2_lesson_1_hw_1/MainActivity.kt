0ac5a13ca06e2240b6d9810fb7140802a1d921d7
VD <vdfree448@protonmail.com>
2022-06-30 13:46:45 +0300

    First commit

--- app/src/main/java/com/example/modul_2_lesson_1_hw_1/MainActivity.kt
+++ app/src/main/java/com/example/modul_2_lesson_1_hw_1/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.modul_2_lesson_1_hw_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MYLOG", "First commit")
+
     }
 }
