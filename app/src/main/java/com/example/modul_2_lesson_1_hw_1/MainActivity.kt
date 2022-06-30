e141bcc7d33d98084b9154c1055d92301baace88
VD <vdfree448@protonmail.com>
2022-06-30 13:47:02 +0300

    Second commit

--- app/src/main/java/com/example/modul_2_lesson_1_hw_1/MainActivity.kt
+++ app/src/main/java/com/example/modul_2_lesson_1_hw_1/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d("MYLOG", "First commit")
+        Log.d("MYLOG", "Second commit")
 
     }
 }
